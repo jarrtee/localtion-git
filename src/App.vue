67a22d8308be1ed63db3bc1319378a423f6bebb4
jarrett <895152506@qq.com>
2024-06-19 16:55:50 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav style="text-align: left;">
+      | <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/other">Other</router-link> |
     </nav>
     <router-view/>
   </div>
---
+++ src/views/OtherView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="other">
+    <h1 style="text-align: left;">!!!!0.0</h1>
+    <el-button type="primary">主要按钮</el-button>
+    </div>
+</template>   
+
+
+<!-- <style scoped>
+.other {
+  display: grid;
+  grid-template-columns: auto auto; /* 定义两列 */
+  gap: 10px; /* 控制列间距 */
+}
+</style> -->
